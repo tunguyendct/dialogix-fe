6f35f7cf93ce41de1665b54cb0772cd4f7077c04
tunguyendct <nguyentudct@gmail.com>
2025-07-04 22:05:15 +0700

    Config develop environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  const PORT: string = env.PORT || '5000';
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    server: {
+      port: parseInt(PORT, 10),
+    },
+  };
 });
